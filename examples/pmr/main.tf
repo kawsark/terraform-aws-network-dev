2f9a6d393bbbd07d2e024b4bc3a51ab03842ff70
kawsark <kawsar@hashicorp.com>
2019-09-14 10:46:25 -0400

    updated to terraform 0.12

--- examples/pmr/main.tf
+++ examples/pmr/main.tf
@@ -1,11 +1,11 @@
 variable "owner" {
   description = "An Owner tag"
-  default = "demouser"
+  default     = "demouser"
 }
 
 variable "security_group_ingress" {
- description = "trusted range for SSH access"
- default = ["1.1.1.1/32"]
+  description = "trusted range for SSH access"
+  default     = ["1.1.1.1/32"]
 }
 
 variable "aws_region" {
@@ -14,34 +14,36 @@ variable "aws_region" {
 
 provider "aws" {
   # AWS provider configured via environment variables: AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 module "aws_network_dev" {
   source = "app.terraform.io/kawsar-org/network-dev/aws"
-#  source = "../../"
-  aws_region = "${var.aws_region}"
-  owner = "${var.owner}"
-  security_group_ingress = "${var.security_group_ingress}"
+
+  #  source = "../../"
+  aws_region             = var.aws_region
+  owner                  = var.owner
+  security_group_ingress = var.security_group_ingress
 }
 
 # Exposing outputs from aws_network_dev module
 output "security_group_id" {
-  value = "${module.aws_network_dev.security_group_id}"
+  value = module.aws_network_dev.security_group_id
 }
 
 output "public_subnet1_id" {
-  value = "${module.aws_network_dev.public_subnet1_id}"
+  value = module.aws_network_dev.public_subnet1_id
 }
 
 output "public_subnet2_id" {
-  value = "${module.aws_network_dev.public_subnet2_id}"
+  value = module.aws_network_dev.public_subnet2_id
 }
 
 output "private_subnet1_id" {
-  value = "${module.aws_network_dev.private_subnet1_id}"
+  value = module.aws_network_dev.private_subnet1_id
 }
 
 output "private_subnet2_id" {
-  value = "${module.aws_network_dev.private_subnet2_id}"
+  value = module.aws_network_dev.private_subnet2_id
 }
+
