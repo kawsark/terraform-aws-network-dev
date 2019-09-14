a9a3f702a26264c808fdd22a978584688c7a1d60
kawsark <kawsar@hashicorp.com>
2019-09-14 12:58:09 -0400

    updated security group ingress variable association

--- main.tf
+++ main.tf
@@ -137,11 +137,10 @@ resource "aws_security_group_rule" "ingress_allow_self" {
 
 resource "aws_security_group_rule" "ingress_allow_ssh" {
   type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = var.security_group_ingress
   security_group_id = aws_security_group.mvd-sg.id
 }
 
